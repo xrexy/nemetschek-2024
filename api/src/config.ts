4ff31e3bee224b676aeae4272d2f04cf477c98a5
xrexy <rexydoesimportantthings@gmail.com>
2024-02-24 23:22:55 +0200

    chore: change api port

--- api/src/config.ts
+++ api/src/config.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 const envSchema = z.object({
   LOG_LEVEL: z.enum(['debug', 'info', 'warn', 'error']).default('info'),
   NODE_ENV: z.enum(['development', 'production']).default('development'),
+  PORT: z.string().default('8080'),
 })
 
 export const env = envSchema.parse(import.meta.env)
--- api/src/index.ts
+++ api/src/index.ts
@@ -2,10 +2,11 @@ import { Elysia } from "elysia";
 
 import { api } from './controllers/*'
 import { ws } from './ws/handler'
+import { config } from "./config";
 const app = new Elysia()
   .use(ws)
   .use(api)
-  .listen(3000);
+  .listen(config.env.PORT);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
