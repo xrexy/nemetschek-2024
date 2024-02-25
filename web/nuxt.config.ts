00a83803031f86d4f4578df819234448d80702be
xrexy <rexydoesimportantthings@gmail.com>
2024-02-25 22:30:20 +0200

    even more hacks

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const production = process.env.NODE_ENV === 'production'
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxtjs/google-fonts", "nuxt-icon", '@formkit/nuxt'],
@@ -8,8 +9,9 @@ export default defineNuxtConfig({
   ssr: false,
   runtimeConfig: {
     public: {
-      apiUrl: process.env.API_URL ||  'http://localhost:8080/api/v1',
-      wsUrl: process.env.WS_URL ||  'ws://localhost:8080/ws',
+      apiUrl: process.env.NT_API_URL ||  production ? 'https://api-production-nemetschek-rkolev.up.railway.app/api/v1' : 'http://localhost:8080/api/v1',
+      wsUrl: process.env.NT_WS_URL ||  production ? 'wss://api-production-nemetschek-rkolev.up.railway.app/ws' : 'ws://localhost:8080/ws',
+      production
     }
   },
   vue: {
